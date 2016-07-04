23b086d54b13dafb2b637722b40d7860c1eb65df
komalgadhave7 <komalgadhave7@gmail.com>
2016-07-03 22:28:05 -0700

    Create Homepage.php
    Main page of the website

---
+++ Homepage.php
@@ -0,0 +1,36 @@
+
+<html>
+
+	<head>	
+	  <title>Homepage</title>
+	  <link href="style.css" rel="stylesheet" type="text/css"/>
+	  <?php include "header.php" ?>
+	  <style>
+	  
+	#div3{
+		width:1200px;
+		height:200px;
+		background-color:;
+		font-size:50px;
+		margin-top:50px;
+		text-align:right;
+}
+	  
+</style>
+
+	</head>
+	<body>
+	  
+	  
+<center>
+	 <div id="div3" style="color:#9900cc">
+		<strong>Welcome To Twitter Timeline</strong>
+		<marquee direction="left" scrollamount="0" behaviour="alternate" width="100px" height="100px"></marquee>
+		<marquee><img src="images11.png" width="400px" height="250px"> <img src="images13.jpg" width="400px" height="250px"> 
+		</marquee>
+
+	</div>
+</center>
+	 <?php include "footer.php" ?>
+	</body>
+</html>
