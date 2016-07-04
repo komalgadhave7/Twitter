e97164607d46aff2a1a7c551ea5125958c6172a0
komalgadhave7 <komalgadhave7@gmail.com>
2016-07-03 22:28:53 -0700

    Create Aboutus.php
    About the website

---
+++ Aboutus.php
@@ -0,0 +1,17 @@
+<html>
+	<title>About</title>
+	  <?php include "header.php" ?>
+	<body>
+		<table align="center" border="1px" height="350px" width="700px">
+			<tr>
+				<td style="color:green"> <p>&nbsp&nbsp&nbsp Twitter is a global community.We ensure the highest level of communication and satisfaction through a deep-set commitment to our clients.Its a global network of expressing and delivery of thoughts.</p>
+	<p>&nbsp&nbsp&nbsp.Our continued industry-leading growth is a testament to the certainty our clients experience every day.  </p> <p>
+
+</p><p>We believe our responsibilities.
+That is why we established twitter- to provide assistance to some of the more socially and economically depressed sectors of the communities.</p></td>
+			</tr>
+		</table>
+       	<?php include "footer.php" ?>
+
+	</body>
+</html>
