376be58715fd31eb50610db483291fe67b47d27d
komalgadhave7 <komalgadhave7@gmail.com>
2016-07-03 22:32:15 -0700

    Create Login.php
    Login to the account

---
+++ Login.php
@@ -0,0 +1,33 @@
+<html>
+
+	<head>
+		<title>Login Form</title>
+<?php include "header.php" ?>
+
+		<style>	
+  
+	
+      </style>
+	</head>
+	<body>
+		
+		<center>
+		<h1 style="color:#9900cc">Login Form</h1>
+		<hr width = "50%">
+		<form action="http://www.twitter.com" target="get">
+		<table border="0px">
+			<tr><td>Username</td><td><input type="text" name="user"></td></tr>
+			<tr><td>Password</td><td><input type="password" name="pwd"></td></tr>
+			<tr align="center"><td colspan="2"><input type="submit" value="Login"></td></td>
+	  
+		</table>
+		<center style="margin-top:10px">
+		<center>
+		</form>
+		</center>
+		<?php include "footer.php" ?>
+
+	
+	</body>
+
+</html>
