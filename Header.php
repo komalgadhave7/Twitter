bcca3cff8a2256af3e977cac6571ad22b068f3fb
komalgadhave7 <komalgadhave7@gmail.com>
2016-07-03 22:31:14 -0700

    Create Header.php
    Header

---
+++ Header.php
@@ -0,0 +1,51 @@
+
+<html>
+<head>
+<script type="text/javascript" src="Script.js"></script>
+<style>
+	  
+#div1{
+	border:1px solid;
+	height:100px;
+     margin:5px;
+	font-size:60px;
+	background-color:#099;
+	text-align:left;
+     }
+
+	#div2{
+		  width:1000px;
+		  margin:20px;
+		  padding-top:1px;
+		  padding-bottom:1px;
+		  font-size:20px;
+		  
+	        }
+	    .navitem{
+			display:inline;
+			padding-right:50px;
+			padding-left:50px;
+			border-right:1px solid red;
+		    }
+</style>
+</head>
+<body >
+	  <center>
+		<div id="container">
+		  <div id="div1"><img src="images12.jpg" alt="collagelogo" align="center"  width="300px" height="70px" style="padding-top:5px;padding-bottom:5px;padding-left:100px;padding-right:115px;"/>Twitter Timeline</div>
+		
+	  </center>
+
+<center>
+	  <div id="div2" bgcolor="ff99ff">  <!--horizontal navigation start-->
+	    <div class="navitem"><a href="Homepage.php">Home</a></div>
+	    <div class="navitem"><a href="Aboutus.php">About Us</a></div>
+	    <div class="navitem"><a href="Contact.php">Contact</a></div>
+	    <div class="navitem" ><a href="Login.php">Login</a></div>
+		
+		
+	  </div>  <!--horizontal navigation end-->
+	 </center>
+	 <hr width = "70%">
+</body>
+</html>
