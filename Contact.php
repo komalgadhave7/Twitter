5acb29877f4e5c8beaa61fd6482da482c3e31ce0
komalgadhave7 <komalgadhave7@gmail.com>
2016-07-03 22:30:17 -0700

    Create Contact.php
    Contact if any problem

---
+++ Contact.php
@@ -0,0 +1,21 @@
+<html>
+	<title>About</title>
+	<?php include "header.php" ?>
+	<body>
+	<h1 align="center" style="color:">Contact Us</h1>
+	<p align="center" style="color:green">
+	<strong><i>Komal Gadhave</i></strong><br>
+	<br></p>
+
+	
+	<br>
+	<hr width="80%">
+	<p align="center" style="color:green"></style><br><br>	<strong>
+	Komal Gadhave:9890471576</strong><br>Email:komalgadhave7@gmail.com<br><br><strong>
+	
+	<br>
+	<hr width="80%">
+	<p align="center" style="color:blue"><strong>Twitter</strong><br>Visit:twitter.com</p>
+	<?php include "footer.php" ?>
+	</body>
+</html>
