b90cbd1cb5b5193ee3b5b3252115d282ccb2e6a3
komalgadhave7 <komalgadhave7@gmail.com>
2016-07-03 22:30:49 -0700

    Create Footer.php
    Footer

---
+++ Footer.php
@@ -0,0 +1,17 @@
+
+<html>
+<style>
+	#div4{
+	border:1px solid;
+	height:75px;
+     margin-top:350px;
+	background-color:#099;
+     }
+
+</style>	  
+<body>
+	<center>
+		<div id="div4">All Rights Reserved &copy Twitter<br> Developed by: Twitter</div>
+	 </center> 
+</body>
+</html>
